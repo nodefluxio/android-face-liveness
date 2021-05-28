9d4fac6d61dcf676b6796acf94e483b93b2f5631
billy <billysutomo.53@gmail.com>
2021-05-28 15:03:35 +0700

    result page

--- app/src/main/java/nodeflux/sample/faceliveness/MainActivity.java
+++ app/src/main/java/nodeflux/sample/faceliveness/MainActivity.java
@@ -1,44 +1,46 @@
 package nodeflux.sample.faceliveness;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.Group;
-import androidx.core.app.ActivityCompat;
-
-import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import java.util.Objects;
 
-import nodeflux.sdk.liveness.Liveness;
-
 public class MainActivity extends AppCompatActivity {
-    private static final int CAMERA_PERMISSION_CODE = 100;
-    private static final int STORAGE_PERMISSION_CODE = 101;
-    Intent intent;
-//    Group intro;
-//    Group success;
-//    Group fail;
-    Button startBtn;
-    Boolean isFirst = true;
 
+    private final int PERMISSION_ALL = 1;
+    String[] PERMISSIONS = {
+            android.Manifest.permission.CAMERA,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+    };
+    Button startBtn;
     TextView message;
 
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        intro = findViewById(R.id.intro);
-//        success = findViewById(R.id.success);
-//        fail = findViewById(R.id.fail);
         startBtn = findViewById(R.id.btn_start);
         message = findViewById(R.id.message);
 
@@ -48,118 +50,18 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-//        setView(0, null);
-
-        intent = new Intent(MainActivity.this, Liveness.class);
-        intent.putExtra("ACCESS_KEY", "{ACCESS_KEY}");
-        intent.putExtra("SECRET_KEY", "{SECRET_KEY}");
-        intent.putExtra("THRESHOLD", 0.7);
-        Liveness.setUpListener(new Liveness.LivenessCallback() {
-            @Override
-            public void onSuccess(boolean isLive, Bitmap bitmap, double score) {
-                if (isLive) {
-//                    setView(1, null);
-                } else {
-//                    setView(2, "Let's try again to check your liveness");
-                }
-                Toast.makeText(getApplicationContext(), String.valueOf(score), Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onError(String message) {
-//                setView(2, message);
-//                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
-            }
-        });
+        if (!hasPermissions(this, PERMISSIONS)) {
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+        }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     public void onStartPressed(View v) {
-        Intent myIntent = new Intent(MainActivity.this, TncActivity.class);
-        MainActivity.this.startActivity(myIntent);
-    }
-
-    public void onLivenessPressed(View v) {
-        isFirst = false;
-        if (!isCameraPermissionGranted()) {
-            requestCameraPermission();
+        if (!hasPermissions(this, PERMISSIONS)) {
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
         } else {
-            startActivity(intent);
-        }
-
-    }
-//
-//    public void setView(int state, String errMessage) {
-//        if (state == 1) {
-//            intro.setVisibility(View.GONE);
-//            success.setVisibility(View.VISIBLE);
-//            fail.setVisibility(View.GONE);
-//            if (!isFirst) {
-//                startBtn.setText("Start Over");
-//                message.setText(errMessage);
-//            }
-//
-//        } else if (state == 2) {
-//            intro.setVisibility(View.GONE);
-//            success.setVisibility(View.GONE);
-//            fail.setVisibility(View.VISIBLE);
-//            startBtn.setText("Try Again");
-//            message.setText(errMessage);
-//        } else {
-//            intro.setVisibility(View.VISIBLE);
-//            success.setVisibility(View.GONE);
-//            fail.setVisibility(View.GONE);
-//            message.setText(errMessage);
-//        }
-//    }
-
-    public boolean isCameraPermissionGranted() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return true;
-        }
-    }
-
-    public void requestCameraPermission() {
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if (requestCode == CAMERA_PERMISSION_CODE) {
-            // Checking whether user granted the permission or not.
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(MainActivity.this,
-                        "Camera Permission Granted",
-                        Toast.LENGTH_LONG)
-                        .show();
-            } else {
-                Toast.makeText(MainActivity.this,
-                        "Camera Permission Denied",
-                        Toast.LENGTH_LONG)
-                        .show();
-            }
-        } else if (requestCode == STORAGE_PERMISSION_CODE) {
-            if (grantResults.length > 0
-                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(MainActivity.this,
-                        "Storage Permission Granted",
-                        Toast.LENGTH_LONG)
-                        .show();
-            } else {
-                Toast.makeText(MainActivity.this,
-                        "Storage Permission Denied",
-                        Toast.LENGTH_LONG)
-                        .show();
-            }
+            Intent myIntent = new Intent(MainActivity.this, TncActivity.class);
+            MainActivity.this.startActivity(myIntent);
         }
     }
 }
--- app/src/main/java/nodeflux/sample/faceliveness/TncActivity.java
+++ app/src/main/java/nodeflux/sample/faceliveness/TncActivity.java
@@ -1,13 +1,20 @@
 package nodeflux.sample.faceliveness;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.Objects;
 
+import nodeflux.sdk.liveness.Liveness;
+
 public class TncActivity extends AppCompatActivity {
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +26,51 @@ public class TncActivity extends AppCompatActivity {
         } catch (NullPointerException e) {
             e.printStackTrace();
         }
+
+        intent = new Intent(TncActivity.this, Liveness.class);
+        intent.putExtra("ACCESS_KEY", "{ACCESS_KEY}");
+        intent.putExtra("SECRET_KEY", "{SECRET_KEY}");
+        intent.putExtra("THRESHOLD", 0.7);
+        Liveness.setUpListener(new Liveness.LivenessCallback() {
+            @Override
+            public void onSuccess(boolean isLive, Bitmap bitmap, double score) {
+                String rootPath = getExternalCacheDir().toString();
+                String filePath = rootPath + "/face.png";
+                File root = new File(rootPath);
+                File file = new File(filePath);
+                try {
+                    if (!root.exists()) {
+                        root.mkdirs();
+                    }
+                    FileOutputStream outStream = new FileOutputStream(file);
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 85, outStream);
+                    outStream.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                callResultActivty(true, "success", score, filePath);
+            }
+
+            @Override
+            public void onError(String message) {
+                callResultActivty(false, message, 0.0, null);
+            }
+        });
     }
 
     public void onLivenessPressed(View v) {
+        startActivity(intent);
     }
+
+    public void callResultActivty(Boolean success, String message, Double score, String imagePath) {
+        Intent myIntent = new Intent(TncActivity.this, ResultActivity.class);
+        myIntent.putExtra("success", success);
+        myIntent.putExtra("message", message);
+        myIntent.putExtra("score", score);
+        myIntent.putExtra("imagePath", imagePath);
+        TncActivity.this.startActivity(myIntent);
+    }
+
 
     public void onBackPressed(View v) {
         finish();
